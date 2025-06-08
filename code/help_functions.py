e80f2815a414d90d26270104ff9f06551c99204f
Peter Steiglechner <peter.steiglechner@gmail.com>
2025-06-08 14:58:10 +0200

    updated the dynamic NoB replica

--- code/help_functions.py
+++ code/help_functions.py
@@ -2,111 +2,13 @@
 
 import numpy as np
 import pandas as pd
-import pingouin
+#import pingouin
 from itertools import combinations
 import copy
 import string
 from scipy.sparse import csr_matrix
 
-def determine_agentlist(filepath, waves, n, atts, seed):
-    """determine set of valid participants in the dataset for all wave regimes 
-
-        determine a list of n participants from a panel dataset that ensures that each selected participant has valid answers for the variables in atts in each element of waves. 
-
-    Args:
-        filepath (string): path to the panel dataset
-        waves (list): a list of wave regimes that contain either a single wave or a list of waves, e.g. waves = [1,19] or waves = [["a", "b"], ["c", "d"]]
-        n (int): selected participants 
-        atts (list): list of opinion dimensions used to determine whether the participant gave valid answers
-        seed (int): random seed
-
-    Returns:
-        list: a list of IDs of participants that have valid responses in each element of waves 
-    """
-    np.random.seed(seed)
-    inds = []
-    for w in waves:
-        df = pd.read_csv(filepath, sep=",", index_col="ID", low_memory=False)
-        if "gesis" in filepath:
-            renameAtts = dict(zip( ["1-kpx_1090", "kpx_1130", "1-kpx_1210", "kpx_1290", "kpx_1250", "kpx_1590"],  ["econ", "migr", "assim", "clim", "euro", "femin"]))
-            attOriginalNames = [c for c in df.columns if (len(c.split(" "))==3) and (c.split(" ")[2][1:-1] in renameAtts.keys()) and not "Imp:" in c]
-            df = df.rename(columns=dict(zip(attOriginalNames, [renameAtts[c.split(" ")[2][1:-1]] for c in attOriginalNames[:]] )))
-        
-        if type(w)==int or type(w)==str:
-            # single wave
-            df = df.loc[df.wave==w]
-        elif type(w)==list:
-            # multiple waves. average per individual (level=0, same index) and take the first entry, which should be the same for all ("the first")
-            df = df.loc[df.wave.isin(w)]
-            for att in atts:
-                df[att] = df.reset_index().groupby("ID")[att].mean()
-            df = df.loc[~df.index.duplicated(keep='first')]
-        
-        ops = df[atts].dropna(how="any", axis="index")
-        inds.append(ops.index.to_list())
-    common = list(set(inds[0]) & set(inds[1]))
-    agentlist = np.random.choice(common, size=n, replace=False)
-    assert (len(agentlist)==n)
-    return agentlist
-
-
-def initialise(filepath, wave, n, predefined_agentlist, atts, seed=42):
-    """initialise agents from a panel dataset
-
-    if n is "all", we use all valid participants in the wave of the panel data 
-    if predefined_agentlist is a list of participant IDs, we use those as agents
-    if predefined_agentlist is None and n is an integer, we randomly sample agents from the valid participants
-
-    Args:
-        filepath (string): path to the panel dataset
-        wave (string, int or list): either single wave or list of waves in the dataset
-        n (int): number of agents
-        predefined_agentlist (None or list of int): None or list of integers
-        atts (list): variables in the dataset 
-        seed (int, optional): random state, only needed if we sample. Defaults to 42.
-
-    Returns:
-        list: list of participant ids
-        pandas.Series: weights for each agent
-        pandas.DataFrame: opinion values for each attitude in atts for each agent 
-        pandas.Series: identity for each agent
-    """
-
-    #prepare dataset 
-    data = pd.read_csv(filepath, sep=",", index_col="ID", low_memory=False)
-    if "gesis" in filepath:
-        renameAtts = dict(zip( ["1-kpx_1090", "kpx_1130", "1-kpx_1210", "kpx_1290", "kpx_1250", "kpx_1590"],  ["econ", "migr", "assim", "clim", "euro", "femin"]))
-        attOriginalNames = [c for c in data.columns if (len(c.split(" "))==3) and (c.split(" ")[2][1:-1] in renameAtts.keys()) and not "Imp:" in c]
-        
-        data = data.rename(columns=dict(zip(attOriginalNames, [renameAtts[c.split(" ")[2][1:-1]] for c in attOriginalNames[:]] )))
-
-    if type(wave)==int or type(wave)==str:
-        # single wave
-        data = data.loc[data.wave==wave]
-    elif type(wave)==list:
-        # multiple waves. average per individual (level=0, same index) and take the first entry, which should be the same for all ("the first")
-        data = data.loc[data.wave.isin(wave)]
-        for att in atts:
-            data[att] = data.reset_index().groupby("ID")[att].mean()
-        data = data.loc[~data.index.duplicated(keep='first')]
-    
-    ops = data[atts].dropna(how="any", axis="index")
-    
-    if not n=="all" and predefined_agentlist is not None:
-        ops = ops.loc[predefined_agentlist]
-    if not n=="all" and predefined_agentlist is None:
-        ops = ops.sample(n, replace=False, random_state=seed)
-    agentlist = ops.index.tolist()
-
-    # extract identity and weights
-    identity = data.loc[agentlist, "partyIdent"]
-    if "ess" in filepath:
-        weights = data.loc[agentlist, "anweight"]
-    else:
-        weights = pd.Series(np.ones(len(ops)), index=ops.index)
-    return agentlist, weights, ops, identity
-
-
+hebbianV = lambda wij, vi, vj, eps:  eps * (1 - abs(wij)) * vi * vj
 
 hebbian = lambda wij, xi, xj, eps: eps * (1 - np.sign(xi * xj) * wij) * xi *xj
 
@@ -164,13 +66,6 @@ def get_socialOmega(agentdict, agentlist, ops, atts, params):
     
 
 
-
-import numpy as np
-import pandas as pd
-from scipy.sparse import csr_matrix
-from itertools import combinations
-
-
 def initialise_socialnetwork(agent_list, identity, opinions, params):
     """
     Initialise a social network of agents with specified identity, opinions, and network structure.
@@ -183,8 +78,8 @@ def initialise_socialnetwork(agent_list, identity, opinions, params):
             - "socInfType": Type of social influence ("correlation", "co-occurence", "copy")
             - "socNetType": Network structure type ("observe-all", "observe-neighbours")
             - "parties": List of identity groups. Excluding "none" (non-partisans).
-            - "indegree": Expected in-group connection probability.
-            - "outdegree": Expected out-group connection probability.
+            - "withinClusterP": in-group link probability.
+            - "betweenClusterP": between-group link probability.
             - "initial_w": Initial weight for belief network edges.
             - "seed": Random seed for reproducibility.
 
@@ -197,62 +92,35 @@ def initialise_socialnetwork(agent_list, identity, opinions, params):
     soc_inf_type = params["socInfType"]
     soc_net_type = params["socNetType"]
     parties = params["parties"]
-    indegree = params["indegree"]
-    outdegree = params["outdegree"]
+    withinClusterP = params["withinClusterP"]
+    betweenClusterP = params["betweenClusterP"]
     initial_weight = params["initial_w"]
+    n_agents = len(agent_list)
+    group_sizes = [(identity == party).sum() for party in parties]
+    # Generate base adjacency matrix for non partisan agents
+    A = (np.random.random((n_agents, n_agents)) <= betweenClusterP).astype(bool)
 
-    if soc_inf_type == "correlation":
-        if soc_net_type != "observe-all":
-            raise ValueError("Correlation-based influence requires 'observe-all' network type.")
-        neighbours = {agent: [] for agent in agent_list}
+    # Create in-group ties
+    start_idx = 0
+    for group_size in group_sizes:
+        end_idx = start_idx + group_size
+        A[start_idx:end_idx, start_idx:end_idx] = (
+            np.random.random((group_size, group_size)) <= withinClusterP
+        )
+        start_idx = end_idx
 
-    elif soc_inf_type in {"co-occurence", "copy"}:
-        if soc_net_type == "observe-all":
-            neighbours = {agent: [] for agent in agent_list}
-
-        elif soc_net_type == "observe-neighbours":
-            n_agents = len(agent_list)
-            group_sizes = [(identity == party).sum() for party in parties]
-            n_partisan = sum(group_sizes)
-
-            # Account for none, nan, other group identities
-            group_sizes.append((identity == "none").sum() + identity.isna().sum() + (identity == "other").sum())
-
-            assert sum(group_sizes) == n_agents, "Group size mismatch."
-
-            # Generate base adjacency matrix for non partisan agents
-            A = (np.random.random((n_agents, n_agents)) <= ((indegree+outdegree) / n_agents)).astype(bool)
-
-            # change to out-group degree for partisan agents
-            if n_partisan>0:
-                A[:n_partisan, :n_partisan] = (np.random.random((n_partisan, n_partisan)) <= ((outdegree) / n_partisan)).astype(bool)
-
-                # Create in-group ties
-                start_idx = 0
-                for group_size in group_sizes[:-1]:
-                    end_idx = start_idx + group_size
-                    A[start_idx:end_idx, start_idx:end_idx] = (
-                        np.random.random((group_size, group_size)) <= (indegree / group_size)
-                    )
-                    start_idx = end_idx
-
-            # Sort agent list for group order
-            agent_list = [
-                agent for party in parties for agent in agent_list if identity[agent] == party
-            ] + [
-                agent for agent in agent_list if identity[agent]=="none" or identity[agent] == "other" or pd.isna(identity[agent])
-            ]
+    # Sort agent list for group order
+    agent_list = [
+        agent for party in parties for agent in agent_list if identity[agent] == party
+    ] + [
+        agent for agent in agent_list if identity[agent]=="none" or identity[agent] == "other" or pd.isna(identity[agent])
+    ]
             
-            A_sparse = csr_matrix(np.triu(A, k=1))
-            neighbours = {
-                agent: [agent_list[nb] for nb in A_sparse[i].indices]
-                for i, agent in enumerate(agent_list)
-            }
-
-        else:
-            raise ValueError(f"Unsupported network type: {soc_net_type}")
-    else:
-        raise ValueError(f"Unsupported influence type: {soc_inf_type}")
+    A_sparse = csr_matrix(np.triu(A, k=1))
+    neighbours = {
+        agent: [agent_list[nb] for nb in A_sparse[i].indices]
+        for i, agent in enumerate(agent_list)
+    }
 
     # Build agent dictionary
     agent_dict = {}
@@ -261,9 +129,10 @@ def initialise_socialnetwork(agent_list, identity, opinions, params):
             (att1, att2): initial_weight for att1, att2 in combinations(atts, 2)
         }
         opinion_vector = opinions.loc[agent].to_dict()
-
         agent_dict[agent] = {
             "x": opinion_vector,
+            "v": dict(zip(atts, np.zeros_like(atts))),
+            "velo_past":[],
             "identity": identity[agent],
             "BN": belief_network,
             "coherence": 0,
@@ -359,6 +228,17 @@ def glauber_probabilities(att, options, beliefs, BN_ag, Temp, atts):
 
 
 
+def social_energy(belief, social_beliefs, social_edge_weight):
+    return np.sum([
+        - social_edge_weight * belief * sb for sb in social_beliefs
+    ])
+def energy(beliefs, att, atts, BN_ag ):
+    return np.sum([
+        -BN_ag[(a1, a2)] * beliefs[a1] * beliefs[a2]
+        for a1, a2 in combinations(atts, 2)
+        if a1 == att or a2 == att
+    ])
+
 
 def glauber_probabilities_withSocial(att, options, beliefs, BN_ag, Temp, atts, social_beliefs, social_edge_weight, beta_pers=1, beta_soc=1):
     """
@@ -378,20 +258,9 @@ def glauber_probabilities_withSocial(att, options, beliefs, BN_ag, Temp, atts, s
     - ps: Array of transition probabilities for each option.
     """
 
-    def social_energy(belief, social_beliefs, social_edge_weight):
-        return np.sum([
-            - social_edge_weight * belief * sb for sb in social_beliefs
-        ])
-    def energy(beliefs):
-        return np.sum([
-            -BN_ag[(a1, a2)] * beliefs[a1] * beliefs[a2]
-            for a1, a2 in combinations(atts, 2)
-            if a1 == att or a2 == att
-        ])
-
     # Original energy
     original_value = beliefs[att]  # Save original to restore later
-    H0 = energy(beliefs)
+    H0 = energy(beliefs, att, atts, BN_ag )
     H_soc_0 = social_energy(original_value, social_beliefs, social_edge_weight)
 
     # Energy for each option
@@ -399,12 +268,14 @@ def glauber_probabilities_withSocial(att, options, beliefs, BN_ag, Temp, atts, s
     H_soc = []
     for opt in options:
         beliefs[att] = opt
-        H.append(energy(beliefs))
+        H.append(energy(beliefs, att, atts, BN_ag))
         H_soc.append(social_energy(opt, social_beliefs, social_edge_weight))
     beliefs[att] = original_value  # Restore original value
 
+    beta_pers = 1/Temp
+    beta_soc = 1/Temp
     delH = beta_pers * (np.array(H)  - H0) + beta_soc * (np.array(H_soc) - H_soc_0)
-    exp_term = 1 / (1+np.exp(delH / Temp))
+    exp_term = 1 / (1+np.exp(delH))
     ps = exp_term / np.sum(exp_term)
     
     return ps
